50738d49c7e9078e591c007927110026cf86f58c
AKHMAD NAUFAL REFANDI <mawikacus@gmail.com>
2026-05-24 06:20:23 +0700

    Update phaselesson-action.blade.php

--- resources/views/phase/phaselesson-action.blade.php
+++ resources/views/phase/phaselesson-action.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex gap-1">
     @php $product_id = request()->route('id'); @endphp
-    <a href="{{ route('phaselesson.form-edit', ['id' => $product_id, 'lesson_id' => $id]) }}" class="btn btn-sm btn-outline-primary" title="Edit">
+    <a href="{{ route('phaselesson.form-edit', ['id' => $product_id, 'id_image' => $id]) }}" class="btn btn-sm btn-outline-primary" title="Edit">
         <i class="bx bx-edit-alt"></i>
     </a>
     <button class="btn btn-sm btn-outline-danger" onclick="confirmDeleteLesson({{ $id }})" title="Hapus">
